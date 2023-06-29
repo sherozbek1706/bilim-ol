9182e77783a3bbde724becb52a960afd837b5b77
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-06-29 18:57:13 +0500

    main buttons link  and button opposite

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,10 +2,8 @@ import { Link } from "react-router-dom";
 import "./button.css";
 export const Button = ({ name, link }) => {
   return (
-    <button className="Button">
-      <Link to={link} className="Button__text">
-        {name}
-      </Link>
-    </button>
+    <Link to={link} className="Button__text">
+      <button className="Button">{name}</button>
+    </Link>
   );
 };
