0cb58e83d3604c123ae8128f40b83d9ac635ac08
sherozbekbaxtiyorov <baxtiyorovsherozbek1111@gmail.com>
2023-07-05 09:54:57 +0500

    Statistic o'zbekcha bo'ldi

--- src/layout/statistic/statistic.jsx
+++ src/layout/statistic/statistic.jsx
@@ -6,15 +6,11 @@ export const Statistic = () => {
     <div className="Main__statistics">
       <div className="Statistic">
         <h1 className="Statistic__number">
-          <CountUp
-            end={Lessons.length}
-            duration={7}
-            className="number_coutr"
-          />
+          <CountUp end={Lessons.length} duration={7} className="number_coutr" />
         </h1>
         <div className="Statistic__rightside">
-          <h2 className="Statistic__name">Lesson</h2>
-          <p className="Statistic__p">All Lessons</p>
+          <h2 className="Statistic__name">Darslar</h2>
+          <p className="Statistic__p">Hamma dars</p>
         </div>
       </div>
       <div className="Statistic">
@@ -22,8 +18,8 @@ export const Statistic = () => {
           <CountUp end={Users.length} duration={10} className="number_coutr" />
         </h1>
         <div className="Statistic__rightside">
-          <h2 className="Statistic__name">Students</h2>
-          <p className="Statistic__p">All Students</p>
+          <h2 className="Statistic__name">O'quvchilar</h2>
+          <p className="Statistic__p">Hamma o'quvchi</p>
         </div>
       </div>
       <div className="Statistic">
@@ -35,8 +31,8 @@ export const Statistic = () => {
           />
         </h1>
         <div className="Statistic__rightside">
-          <h2 className="Statistic__name">Words</h2>
-          <p className="Statistic__p">All Words</p>
+          <h2 className="Statistic__name">So'zlar</h2>
+          <p className="Statistic__p">Hamma so'z</p>
         </div>
       </div>
     </div>
